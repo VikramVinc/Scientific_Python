e5ddc07934166b25b7b48aa5c149d329ea93895a
VikramVinc <110795062+VikramVinc@users.noreply.github.com>
2022-12-08 10:23:48 +0530

    Update challenge134.py

--- challenge134.py
+++ challenge134.py
@@ -1,7 +1,10 @@
+'''Code that finds side of a polygon given
+diagonal of the polygon.'''
+
 from math import sqrt
 T = int(input())
 for _ in range(T):
     diagonal = int(input())
     side = (3+(sqrt(9 + 8*diagonal)))/2
-    print((side).__trunc__())
+    print((side).__ceil__())
 
